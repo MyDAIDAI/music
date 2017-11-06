fdff0a9b18a919afbfbeaad7751005d5ae0e4eb1
daidai <571143755@qq.com>
2017-11-06 17:46:53 +0800

    头部插件设置

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,19 @@
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
     <!-- <router-view/> -->
-    hello, world
+    <m-header></m-header>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import MHeader from 'components/m-header/m-header'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    MHeader
+  }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "~common/stylus/variable"
-
-  #app
-    color: $color-theme
- 
 </style>
