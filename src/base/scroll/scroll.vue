a3ee37050c5e8c67fa19de88d913cb7322816156
daidai <571143755@qq.com>
2018-01-12 16:23:25 +0800

    推荐页面

---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,67 @@
+<template>
+  <div ref="wrapper">
+    <slot></slot>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
+  const DIRECTION_H = 'horizontal'
+  const DIRECTION_V = 'vertical'
+  export default {
+    name: 'scroll',
+    props: {
+      probeType: {
+        type: Number,
+        default: 1
+      },
+      click: {
+        type: Boolean,
+        default: true
+      },
+      data: {
+        type: Array,
+        default: null
+      },
+      direction: {
+        type: String,
+        default: DIRECTION_V
+      }
+    },
+    mounted () {
+      setTimeout(() => {
+        this._initScroll()
+      }, 20)
+    },
+    methods: {
+      _initScroll () {
+        if (!this.$refs.wrapper) {
+          return
+        }
+        this.scroll = new BScroll(this.$refs.wrapper, {
+          probeType: this.probeType,
+          click: this.click,
+          eventPassthrough: this.direction === DIRECTION_V ? DIRECTION_H : DIRECTION_V
+        })
+      },
+      enable () {
+        this.scroll && this.scroll.enable()
+      },
+      disable () {
+        this.scroll && this.scroll.disable()
+      },
+      refresh () {
+        console.log(this.scroll)
+        this.scroll && this.scroll.refresh()
+      }
+    },
+    watch: {
+      data () {
+        console.log(this.data)
+        setTimeout(() => {
+          this.refresh()
+        }, 20)
+      }
+    }
+  }
+</script>
+
