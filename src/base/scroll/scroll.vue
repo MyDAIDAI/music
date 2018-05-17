041c5f2532f4a9c0cb4a72a2a4736b792daabd3f
daidai <571143755@qq.com>
2018-05-17 14:13:18 +0800

    scroll添加pullup事件

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -29,6 +29,14 @@
       listenScroll: {
         type: Boolean,
         default: false
+      },
+      pullup: {
+        type: Boolean,
+        default: false
+      },
+      beforeScroll: {
+        type: Boolean,
+        default: false
       }
     },
     mounted () {
@@ -49,6 +57,13 @@
         if (this.listenScroll) {
           this.scroll.on('scroll', (pos) => {
             this.$emit('scroll', pos)
+          })
+        }
+        if (this.pullup) {
+          this.scroll.on('scrollEnd', () => {
+            if (this.scroll.y <= (this.scroll.maxScrollY + 50)) {
+              this.$emit('scrollToEnd')
+            }
           })
         }
       },
