62cebacda74ad18cb1a9b1514eb8ea563fc1aa52
daidai <571143755@qq.com>
2018-05-17 14:11:09 +0800

    搜索页面

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,11 +1,151 @@
 <template>
-  <div>
-    搜索页面
+  <div class="search">
+    <div class="search-box-wrapper">
+      <search-box ref="searchBox" @query="onQueryChange"></search-box>
+    </div>
+    <div class="shortcut-wrapper" v-show="!query">
+      <scroll class="shortcut" ref="shortcut">
+        <div>
+          <div class="hot-key">
+            <h1 class="title">热门搜索</h1>
+            <ul>
+              <li @click="addQuery(item.k)" class="item" v-for="item in hotKey">
+                <span>{{item.k}}</span>
+              </li>
+            </ul>
+          </div>
+          <div class="search-history">
+            <h1 class="title">
+              <span class="text">搜索历史</span>
+              <span class="clear">
+                <i class="icon-clear"></i>
+              </span>
+            </h1>
+            <!--搜索历史列表-->
+            <search-list :searches="searchHistory" @select="addQuery" @delete="deleteSearchHistory"></search-list>
+          </div>
+        </div>
+      </scroll>
+    </div>
+    <div class="search-result" v-show="query">
+      <suggest :query="query" @select="saveSearch"></suggest>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import SearchBox from 'base/search-box/search-box'
+  import Scroll from 'base/scroll/scroll'
+  import {getHotKey} from 'api/search'
+  import {ERR_OK} from 'api/config'
+  import SearchList from 'base/search-list/search-list'
+  import {mapGetters, mapActions} from 'vuex'
+  import Suggest from 'components/suggest/suggest'
+
+  export default {
+    data () {
+      return {
+        hotKey: [],
+        query: '',
+        refreshDelay: 120
+      }
+    },
+    components: {
+      Scroll,
+      SearchBox,
+      SearchList,
+      Suggest
+    },
+    computed: {
+      ...mapGetters([
+        'searchHistory'
+      ])
+    },
+    created () {
+      this._getHotKey()
+    },
+    methods: {
+      onQueryChange (value) {
+        this.query = value.trim()
+      },
+      addQuery (query) {
+        this.$refs.searchBox.setQuery(query)
+      },
+      saveSearch () {
+        this.saveSearchHistory(this.query)
+      },
+      _getHotKey () {
+        getHotKey().then((res) => {
+          if (res.code === ERR_OK) {
+            this.hotKey = res.data.hotkey.slice(0, 10)
+          }
+        })
+      },
+      ...mapActions([
+        'saveSearchHistory',
+        'deleteSearchHistory'
+      ])
+    },
+    watch: {
+      query (newQuery) {
+        if (!newQuery) {
+          setTimeout(() => {
+            // 历史列表更新
+            this.$refs.shortcut.refresh()
+          }, 20)
+        }
+      }
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+  .search
+    .search-box-wrapper
+      margin: 20px
+    .shortcut-wrapper
+      position: fixed
+      top: 178px
+      bottom: 0
+      width: 100%
+      .shortcut
+        height: 100%
+        overflow: hidden
+        .hot-key
+          margin: 0 20px 20px 20px
+          .title
+            margin-bottom: 20px
+            font-size: $font-size-medium
+            color: $color-text-l
+          .item
+            display: inline-block
+            padding: 5px 10px
+            margin: 0 20px 10px 0
+            border-radius: 6px
+            background: $color-highlight-background
+            font-size: $font-size-medium
+            color: $color-text-d
+        .search-history
+          position: relative
+          margin: 0 20px
+          .title
+            display: flex
+            align-items: center
+            height: 40px
+            font-size: $font-size-medium
+            color: $color-text-l
+            .text
+              flex: 1
+            .clear
+              extend-click()
+              .icon-clear
+                font-size: $font-size-medium
+                color: $color-text-d
+    .search-result
+      position: fixed
+      width: 100%
+      top: 178px
+      bottom: 0
 </style>
