a9856bab0647fb2cd1327930ffde87bc4300b526
daidai <571143755@qq.com>
2018-05-17 14:08:20 +0800

    搜索历史列表

--- src/base/search-list/search-list.vue
+++ src/base/search-list/search-list.vue
@@ -1,16 +1,68 @@
 <template>
-    $END$
+  <div class="search-list" v-show="searches.length">
+    <transition-group name="list" tag="ul">
+      <li class="search-item" v-for="item in searches" @click="selectItem(item)" :key="item">
+        <span class="text">{{item}}</span>
+        <span class="icon" @click="deleteOne(item)">
+          <i class="icon-delete"></i>
+        </span>
+      </li>
+    </transition-group>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'search-list',
-    data() {
+    data () {
       return {}
+    },
+    props: {
+      searches: {
+        type: Array,
+        default: []
+      }
+    },
+    methods: {
+      selectItem (item) {
+        this.$emit('select', item)
+      },
+      deleteOne (item) {
+        this.$emit('delete', item)
+      }
+    },
+    watch: {
+      searches (newVal, oldVal) {
+        console.log(newVal)
+      }
     }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
 
+  .search-list
+    .search-item
+      display: flex
+      align-items: center
+      height: 40px
+      overflow: hidden
+      &.list-enter-active, &.list-leave-active
+        -webkit-transition: all 0.1s
+        -moz-transition: all 0.1s
+        -ms-transition: all 0.1s
+        -o-transition: all 0.1s
+        transition: all 0.1s
+      &.list-enter, &.list-leave-to
+        height: 0
+      .text
+        flex: 1
+        color: $color-text-l
+      .icon
+        extend-click()
+        .icon-delete
+          font-size: $font-size-small
+          color: $color-text-d
 </style>
