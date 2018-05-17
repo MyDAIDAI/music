d5f6263645cd91fa5fd6536b020437d5a65ae3c0
daidai <571143755@qq.com>
2018-05-17 15:06:17 +0800

    修改searchHistoy的Bug

--- src/common/js/cache.js
+++ src/common/js/cache.js
@@ -19,7 +19,7 @@ function insertArray (arr, val, compare, maxLen) {
 function deleteFromArray (arr, compare) {
   const index = arr.findIndex(compare)
   if (index > -1) {
-    arr.split(index, 1)
+    arr.splice(index, 1)
   }
 }
 
